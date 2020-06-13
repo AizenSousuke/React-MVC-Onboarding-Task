f04d4b1a2e77b0085e9aee6a54193627f1d06607
AizenSousuke <puteranikdimas@yahoo.com.sg>
2020-06-13 21:45:07 +0800

    Added Azure

--- MVPStudioReactTemplate/Startup.cs
+++ MVPStudioReactTemplate/Startup.cs
@@ -1,12 +1,11 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using MVPStudioReactTemplate.Context;
 
 namespace MVPStudioReactTemplate
@@ -21,11 +20,20 @@ namespace MVPStudioReactTemplate
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
         {
+
             services.AddDbContext<AppDbContext>(options =>
             {
-                options.UseSqlServer(Configuration.GetConnectionString("Database"));
+                if (env.IsDevelopment())
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("Database"));
+                } else
+                {
+                    // Prod database
+                    options.UseSqlServer(Configuration.GetConnectionString("ProductionDatabase"));
+                }
+
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -50,7 +58,7 @@ namespace MVPStudioReactTemplate
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
